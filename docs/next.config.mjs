41ba610468195758262bd975f06ebe100163ea4f
leochiu <leochiu.life@gmail.com>
2026-01-20 22:39:02 +0800

    feat: enhance documentation layouts with new accordion components and add demo images

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,12 +1,13 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
-  output: 'export',
+  output: "export",
   reactStrictMode: true,
   basePath: process.env.PAGES_BASE_PATH,
+  images: { unoptimized: true },
 };
 
 export default withMDX(config);
