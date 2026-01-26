8dabd82996d9844cc98d8eea58caeaf8f5632ce1
leochiu <leochiu.life@gmail.com>
2026-01-26 20:12:24 +0800

    feat: add metadata for Slidev Theme Gemini in layout and remove unused metadata generation in page

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,10 +1,20 @@
 import { Inter } from 'next/font/google';
 import { Provider } from '@/components/provider';
 import './global.css';
+import { Metadata } from 'next';
 
 const inter = Inter({
   subsets: ['latin'],
 });
+
+export const metadata: Metadata = {
+  title: 'Slidev Theme Gemini',
+  description: 'Slidev Theme Gemini is a theme for Slidev',
+  openGraph: {
+    images: '/cover.webp',
+  },
+  metadataBase: new URL('https://leochiu-a.github.io/slidev-theme-gemini/'),
+};
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
